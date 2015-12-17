7da9a321713f1f8a187eaeba18b7aa5caad3fbe3
David Björklund <david.bjorklund@gmail.com>
2015-12-17 19:11:41 +0100

    fix tests

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ test('YoutubeVideo initial state', function (t) {
       'youtube-id': 'YoB8t0B4jx4'
     },
     state: {
-      videoOpened: false
+      opened: false
     }
   })));
 
@@ -18,6 +18,7 @@ test('YoutubeVideo initial state', function (t) {
     <div class="youtube-video">
       <div>
         <div class="youtube-video__image" style="background-image: url(http://img.youtube.com/vi/YoB8t0B4jx4/hqdefault.jpg);"></div>
+        <div class="youtube-video__play-btn youtube-video__play-btn--hover"></div>
         <div class="youtube-video__play-btn"></div>
       </div>
     </div>`);
@@ -30,12 +31,12 @@ test('YoutubeVideo video opened state', function (t) {
       'youtube-id': 'YoB8t0B4jx4'
     },
     state: {
-      videoOpened: true
+      opened: true
     }
   })));
 
   t.equal(html, tsml`
-    <div class="youtube-video">
+    <div class="youtube-video youtube-video--opened">
       <iframe class="youtube-video__frame" src="http://www.youtube.com/embed/YoB8t0B4jx4?autoplay=1" frameBorder="0"></iframe>
     </div>`);
   t.end();
